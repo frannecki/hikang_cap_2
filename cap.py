a35656f64bd4dd341d3b3457c98c4d9949515405
frannecki <frannecki@users.noreply.github.com>
2018-04-04 18:28:08 +0800

    commit 4/4

--- cap.py
+++ cap.py
@@ -5,32 +5,35 @@ import cv2 as cv
 import keras
 from keras import backend as K
 from keras.models import load_model
+print('model loading...')
+model = load_model('D:/Documents/QT/passpic/flame_model_2.h5')
+print('model loaded.')
 
 
 def pri(A, B, n):
     A = np.asarray(A, dtype=np.uint8) #'np.uint8' declaration required
     A = A.reshape(n, B[0], B[1], B[2])
-    print(A)
+    #print(A)
     print(A.shape)
-    A1 = A[1]
-    cv.imshow("pic", A1)
-    cv.waitKey(0)
+    for i in range(0, n):
+        A1 = A[i]
+        print(A1.shape)
+        cv.imshow("pic", A1)
+        cv.waitKey(1000)
 
 
 def pred(A, B, n):
     A = np.asarray(A, dtype=np.uint8)
     A = A.reshape(n, B[0], B[1], B[2])
     ret = []
-    print('model loading...')
-    model = load_model('D:/Documents/QT/passpic/flame_model_2.h5')
-    print('model loaded.')
     for i in range(0, n):
         A1 = A[i]
         if K.image_data_format() == 'channels_first':
-            A1 = A1.reshape(n, B[2], B[0], B[1])
+            A1 = A1.reshape(1, B[2], B[0], B[1])
         else:
             A1 = A1.reshape(1, B[0], B[1], B[2])
         pred = model.predict_classes(A1)
+        #print('hhh')
         if pred[0] == 1:
             #ret[i] = 1 false statement (list assignment index out of range)
             ret.append(1)
--- readme.txt
+++ readme.txt
@@ -1,2 +1,12 @@
+4/2
 For unknown reasons the child threads are unable to call the python module (error occurs).
 For unknown reasons the main fuction are unable to call pycap() multiple times (error occurs).
+
+4/4
+1. rwlockhead.h中的CRWLock类定义读写锁。在抓图的子线程中，每次向IplImage* 数组中写入数据时采用写锁，
+   此时主线程中被锁的读图代码块挂起；写入完毕后解锁，读图代码执行，此时写入数据的代码块挂起。读写锁针对
+   两线程共享的对象，避免同时进行访问导致内存错误。
+2. 子线程每次抓图后将图片resize成为100x100的BGR三通道图片，深度为8，触发写锁，写入数组（数组容量为30）。
+   解锁后挂起0.2s，继续试图写入。
+3. 主线程预先加载python模型及函数，加载完毕后创建子线程，待存放图片的数组存入30帧图片时触发读锁，开始
+   读取，完毕后解锁，将图片传入python函数用训练好的模型进行识别。挂起1000ms后继续试图读取。
